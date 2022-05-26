dab51382b8ea559a28aa97e85ffa0e9d7a8be555
R. G. Koch <koch.rg@gmail.com>
2022-05-26 13:33:26 -0300

    Add missing settings files

--- api/management/commands/initadmin.py
+++ api/management/commands/initadmin.py
@@ -1,5 +1,6 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from django.core.management.base import BaseCommand
+from utils.bcolors import bcolors
 
 User = get_user_model()
 
@@ -8,10 +9,11 @@ User = get_user_model()
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
+        username = 'admin'
+        password = 'imagesizator'
+
         if not User.objects.filter(username='admin').exists():
-            username = 'admin'
             email = 'email@example.com'
-            password = 'imagesizator'
             print('Creating account for %s (%s)' % (username, email))
             admin = User.objects.create_superuser(email=email, username=username, password=password)
             admin.is_active = True
@@ -19,3 +21,13 @@ class Command(BaseCommand):
             admin.save()
         else:
             print('Admin accounts can only be initialized if no Accounts exist')
+
+            # try to login with username/password
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                # Default admin password, WARNING!!
+                print(f"{bcolors.WARNING}WARNING!! You have the default admin password!!{bcolors.ENDC}")
+                print(
+                    f"{bcolors.WARNING}Change it from Django admin. " 
+                    + f"View the Readme.md file for more details.{bcolors.ENDC}"
+                )
--- imagesizator/settings_prod.py
+++ imagesizator/settings_prod.py
@@ -1,6 +1,17 @@
 from .settings import *  # noqa
 
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+
+# Managing static files in production
+BASE_DIR = Path(__file__).resolve(strict=True).parent
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+STATIC_ROOT = os.path.join(BASE_DIR, "site_static")
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
+MEDIA_URL = "/media/"
+
+STATIC_URL = "/static/"
